c2736139a4a145284dae4ce517d1b47fb358b894
sipan <sipan@st-dev>
2015-12-21 18:05:57 +0400

    nemo cron

--- nemo/apps/payment/management/commands/transaction.py
+++ nemo/apps/payment/management/commands/transaction.py
@@ -1,10 +1,15 @@
 from django.core.management.base import BaseCommand
-
+import datetime
 import os
 from django.db import transaction
+from django.utils import timezone
+from payment.models import Rent
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-            print 'sss'
+        today = timezone.now() - datetime.timedelta(days=1)
+        rents = Rent.objects.filter(status='approved',rent_date__lte=today)
+        for rent in rents:
+            print rent.id
