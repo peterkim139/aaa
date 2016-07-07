b53a606689dee4de1df5ab0b0d8d753c08991357
hrayr <hrayr@stdevmail.com>
2016-07-07 11:59:40 +0400

    st

--- nemo/static/js/menu.js
+++ nemo/static/js/menu.js
@@ -78,24 +78,11 @@
 //});
 
 
-
 $(document).ready(function(){
 	addMobileMenu();
 	$(window).on('resize', addMobileMenu);
 	var status = false;
-	$('#showMenu').on('click', function(){
-		if (!status) {
-			$('body').css('overflow', 'hidden');
-			$("#showMenu i").removeClass("icon-bars").addClass("icon-cancel");
-			$('.headerNavMobile').show();
-			status = true;
-		} else {
-			$('body').css('overflow', 'auto');
-			$("#showMenu i").removeClass("icon-cancel").addClass("icon-bars");
-			$('.headerNavMobile').hide();
-			status = false;
-		}
-	})
+	$('#showMenu').on('click', menuToggle)
 });
 
 function addMobileMenu(){
@@ -105,5 +92,20 @@ function addMobileMenu(){
 		$('#showMenu').appendTo('body');
 	} else if ($(window).width()>=880) {
 		$('.headerNavMobile').remove();
+	}
+	$('.popupBtn').magnificPopup();
+}
+
+function menuToggle(){
+	if (!status) {
+		$('body').css('overflow', 'hidden');
+		$("#showMenu i").removeClass("icon-bars").addClass("icon-cancel");
+		$('.headerNavMobile').show();
+		status = true;
+	} else {
+		$('body').css('overflow', 'auto');
+		$("#showMenu i").removeClass("icon-cancel").addClass("icon-bars");
+		$('.headerNavMobile').hide();
+		status = false;
 	}
 }
